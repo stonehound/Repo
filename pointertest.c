223728b23c985eb3b66a8a65eef0f058035b191b
stonehound <crimecomputermusic@gmail.com>
2025-06-23 00:32:56 -0400

    Second attempt to commit pointertest.c to github repo.

---
+++ pointertest.c
@@ -0,0 +1,10 @@
+//This is a test of using pointers with the malloc() and free() functions from the <stdlib.h> library.
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+//Create a pointer:
+
+
+}
