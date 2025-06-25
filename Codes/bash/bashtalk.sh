dba09f71fafe5291557a368b1bf0b8a9f5ef5432
stonehound <crimecomputermusic@gmail.com>
2025-06-24 20:52:00 -0400

    Consolidating bash code and preparing for new OS flash on stonepile.

---
+++ Codes/bash/bashtalk.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#Test running a C program with BASH.
+
+echo "Begin bastalk.sh"
+
+echo "This will run speech.out:"
+
+./speech.out
+
+echo "End bashtalk.sh"
+
+exit
---
+++ Codes/bash/datetest.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Script meant to tell date and time
+
+echo "The current date and time is `date +%c`"
+
+echo "The number of days since the beginning of the year are `date +%j`"
+
+# echo "The current date is `date`"
+
+exit 0
