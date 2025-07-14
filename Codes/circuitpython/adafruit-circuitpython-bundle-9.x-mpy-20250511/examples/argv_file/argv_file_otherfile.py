2dce0312a7b86f00543171b17b725b658ef902dc
stonehound <crimecomputermusic@gmail.com>
2025-07-14 00:22:00 -0400

    Updating Circuitpython codes.

--- Codes/circuitpython/adafruit-circuitpython-bundle-9.x-mpy-20250511/examples/argv_file/argv_file_otherfile.py
+++
@@ -1,8 +0,0 @@
-# SPDX-FileCopyrightText: Copyright (c) 2025 Tim Cocks for Adafruit Industries
-#
-# SPDX-License-Identifier: Unlicense
-
-from adafruit_argv_file import read_argv
-
-args = read_argv(__file__)
-print(args)
