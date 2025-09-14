fcdd845623e8b8963aa222decc5fa7f9f2966693
stonehound <crimecomputermusic@gmail.com>
2025-09-14 12:39:06 -0400

    Adding project soace for reactive video synthesis with python

---
+++ Codes/python/supriyatest.py
@@ -0,0 +1,42 @@
+# This is a test of Supriya music python library
+
+import time
+import supriya
+
+def main() -> None:
+    """
+    The example entry-point function.
+    """
+    # Create a server and boot it:
+    server = supriya.Server().boot()
+    # Define a C-major chord in Hertz
+    frequencies = [261.63, 329.63, 392.00]
+    # Create an empty list to store synths in:
+    synths: list[supriya.Synth] = []
+    # Start an OSC bundle to run immediately:
+    with server.at():
+        # Add the default synthdef to the server and open a "completion"
+        # context manager to group further commands for when the synthdef
+        # finishes loading:
+        with server.add_synthdefs(supriya.default):
+            # Loop over the frequencies:
+            for frequency in frequencies:
+                # Create a synth using the default synthdef and the frequency
+                # and add it to the list of synths:
+                synths.append(
+                    server.add_synth(synthdef=supriya.default, frequency=frequency)
+                )
+    # Let the notes play for 4 seconds:
+    time.sleep(4)
+    # Loop over the synths and free them
+    for synth in synths:
+        synth.free()
+    # Wait a second for the notes to fade out:
+    time.sleep(1)
+    # Quit the server:
+    server.quit()
+
+if __name__ == "__main__":
+    main()
+
+
