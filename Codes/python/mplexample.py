0ff3a730fe8132fe0b18526138005d2c5da8e1a0
stonehound <crimecomputermusic@gmail.com>
2025-09-17 09:24:05 -0400

    Added comment code to Matplotlib example

--- Codes/python/mplexample.py
+++ Codes/python/mplexample.py
@@ -1,11 +1,19 @@
 # Matplot Example
+"""Libraries for matplotlib and numpy to handle 
+calculations for the function to be plotted
+"""
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Create a dataset ranging from -10 to 10 with 100 steps
 t = np.linspace(-10, 10, 100)
+# Create a function to be plotted
 sig = 1 / (1 + np.exp(-t))
 
+# Setup Plot
+# Create the plot obeject
 fig, ax = plt.subplots()
+# Define axes
 ax.axhline(y=0, color="black", linestyle="--")
 ax.axhline(y=0.5, color="black", linestyle=":")
 ax.axhline(y=1.0, color="black", linestyle="--")
@@ -14,4 +22,5 @@ ax.axline((0, 0.5), slope=0.25, color="black", linestyle=(0, (5, 5)))
 ax.plot(t, sig, linewidth=2, label=r"$\sigma(t) = \frac{1}{1 + e^{-t}}$")
 ax.set(xlim=(-10, 10), xlabel="t")
 ax.legend(fontsize=14)
+# Plot the function
 plt.show()
