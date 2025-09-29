7f35a03e3136b71989cccbacb7d18e79438204aa
stonehound <crimecomputermusic@gmail.com>
2025-09-29 19:54:53 -0400

    Modifying Lotka Volterra model

--- Codes/python/npsci/lotvoltime.py
+++ Codes/python/npsci/lotvoltime.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from scipy import integrate
 import ipywidgets as ipw
 
+# Initiate variables for the parameters and initial conditions
 alpha = 1. #mortality rate due to predators
 beta = 1.
 delta = 1.
@@ -12,19 +13,24 @@ gamma = 1.
 x0 = 4.
 y0 = 2.
 
+# Defined the Lotka-Volterra Equations
 def derivative(X, t, alpha, beta, delta, gamma):
     x, y = X
     dotx = x * (alpha - beta * y)
     doty = y * (-delta + gamma * x)
     return np.array([dotx, doty])
 
+# Create variables to represent time passing in the system
 Nt = 1000
 tmax = 30.
 t = np.linspace(0.,tmax, Nt)
+
+# Set up and compute the Integration
 X0 = [x0, y0]
 res = integrate.odeint(derivative, X0, t, args = (alpha, beta, delta, gamma))
 x, y = res.T
 
+# Plot the models
 plt.figure()
 plt.grid()
 plt.title("odeint method")
